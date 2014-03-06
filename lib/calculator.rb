9f9607c1b63b08b4c078cafe5e5a3fb76a4267db
Mark Lehman <markopolo@gmail.com>
2014-03-06 10:32:54 -0800

    add functionality for addition, subtraction, multiplication, division, and floats

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,29 +1,32 @@
 def calculator(sentence)
-math_word = { "plus" => :+ }
+  operator_words_to_symbols = { 
+    "plus" => :+,
+    "minus" => :-,
+    "times" => :*,
+    "multiplied" => :*,
+    "divided" => :/
+   }
 
-delete_array = ["what", "What", "is", "to", "the", "by"]
-
-operator = ""
+  words_to_delete = ["what", "What", "is", "to", "the", "by"]
 
   sentence.chomp!("?")
-  new_sentence = sentence.split(" ")
-  new_sentence.delete_if { |string| delete_array.include?(string)  }
+  strings_in_sentence = sentence.split(" ")
+  strings_in_sentence.delete_if { |string| words_to_delete.include?(string)  }
   
-  new_sentence.each_with_index do |string, index|
-    if math_word.include?(string)
-      new_sentence[index] = math_word[string]
+  strings_in_sentence.each_with_index do |string, index|
+    if operator_words_to_symbols.include?(string)
+      strings_in_sentence[index] = operator_words_to_symbols[string]
     end
   end
-  operator = new_sentence[1].to_sym
-  new_sentence.delete_at(1)
+  operator = strings_in_sentence[1].to_sym
+  strings_in_sentence.delete_at(1)
   integers = []
-  new_sentence.each do |number|
-    integers << number.to_i
+  strings_in_sentence.each do |number|
+    integers << number.to_f
   end
   integers.inject(operator)
-  # integers
 end
 
-calculator("What is 5 plus 8?")
+calculator("What is 4 divided by 2?")
 
 
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,7 +2,23 @@ require 'rspec'
 require 'calculator'
 
 describe('calculator') do
-  it('takes a string ie "what is 5 plus 8?" and returns the answer as an integer') do
+  it('takes a string ie "what is 5 plus 8?" and returns the answer as a float') do
     calculator("What is 5 plus 8?").should(eq(13))
   end
+
+  it('takes a string ie "what is 9 minus 4?" and returns the answer as a float') do
+    calculator("what is 9 minus 4?").should(eq(5))
+  end
+
+  it('takes a string ie "what is 6 times 3?" and returns the answer as a float') do
+    calculator("what is 6 times 3?").should(eq(18))
+  end
+
+  it('takes a string ie "what is 12 divided by 4?" and returns the answer as a float') do
+    calculator("what is 12 divided by 4?").should(eq(3))
+  end
+
+  it('takes a string ie "what is 12.9 divided by 4.3?" and returns the answer as a float') do
+    calculator("what is 5 divided by 2").should(eq(2.5))
+  end  
 end
